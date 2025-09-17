311199da5af4fab7b57133ab635a001088db28d9
donghyeon505 <ehdgus458@gmail.com>
2025-09-17 14:54:55 +0900

    refactor(todo): 필수기능 2-8 완료 - DSL 의존성 추가 - QueryDslConfig 등록 - findByTodoIdWithUser 메서드 DSL로 리팩토링

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryQueryImpl.java
@@ -0,0 +1,30 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.QTodo;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.QUser;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class TodoRepositoryQueryImpl implements TodoRepositoryQuery {
+
+    private final JPAQueryFactory queryFactory;
+
+    private static final QTodo todo = QTodo.todo;
+    private static final QUser user = QUser.user;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        Todo result = queryFactory
+                .selectFrom(todo)
+                .leftJoin(todo.user, user).fetchJoin()
+                .where(todo.id.eq(todoId))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+}
+
